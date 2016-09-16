dd97eb3bd8b9557c013fdde0c7e5dfd1138c7e9a
Marcin Tkocz <tkocz.marcin@gmail.com>
2016-09-16 17:33:12 +0200

    18

---
+++ js/skrypt.js
@@ -0,0 +1,18 @@
+
+   
+/* scroll \/ */
+jQuery(function($)
+	{
+		$.scrollTo(0);			
+//		$('#home').click(function() { $.scrollTo($('body'), 500); });
+		$('#nav-1').click(function() { $.scrollTo($('#header'), 300); });
+		$('#nav-2').click(function() { $.scrollTo($('#section-two'), 500); });
+		$('#nav-3').click(function() { $.scrollTo($('#section-three'), 500); });
+		$('#nav-4').click(function() { $.scrollTo($('#section-four'), 500); });
+		$('#nav-5').click(function() { $.scrollTo($('#section-five'), 500); });
+		$('#nav-6').click(function() { $.scrollTo($('#section-six'), 500); });
+		$('#nav-7').click(function() { $.scrollTo($('#section-seven'), 500); });
+
+
+	}
+);
