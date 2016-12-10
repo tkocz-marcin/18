9f62d1e07b3c50f02b230358adcb794c3cb0e7fb
Marcin Tkocz <tkocz.marcin@gmail.com>
2016-12-10 14:01:25 +0100

    fix

--- js/skrypt.js
+++ js/skrypt.js
@@ -5,9 +5,9 @@ jQuery(function($)
 	{
 		$.scrollTo(0);			
 //		$('#home').click(function() { $.scrollTo($('body'), 500); });
-		$('#nav-1').click(function() { $.scrollTo($('#header'), 300); });
-		$('#nav-2').click(function() { $.scrollTo($('#section-two'), 500); });
-		$('#nav-3').click(function() { $.scrollTo($('#section-three'), 500); });
+		$('#nav-1').click(function() { $.scrollTo($('body'), 300); });
+		$('#nav-2').click(function() { $.scrollTo($('#section-one'), 500); });
+		$('#nav-3').click(function() { $.scrollTo($('#section-two'), 500); });
 		$('#nav-4').click(function() { $.scrollTo($('#section-four'), 500); });
 		$('#nav-5').click(function() { $.scrollTo($('#section-five'), 500); });
 		$('#nav-6').click(function() { $.scrollTo($('#section-six'), 500); });
